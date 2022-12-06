87093d33efca562a1be6ee686059d3ec03f0f895
Mrucznik <mrucznix@gmail.com>
2022-12-06 21:48:40 +0100

    day 6 part 2 fix tests

--- day6/part1/main_test.go
+++ day6/part1/main_test.go
@@ -3,8 +3,10 @@ package main
 import "testing"
 
 func Test_findMarker(t *testing.T) {
+
 	type args struct {
-		msg string
+		msg       string
+		markerLen int
 	}
 	tests := []struct {
 		name string
@@ -14,42 +16,47 @@ func Test_findMarker(t *testing.T) {
 		{
 			name: "",
 			args: args{
-				msg: "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+				msg:       "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+				markerLen: 4,
 			},
 			want: 7,
 		},
 		{
 			name: "",
 			args: args{
-				msg: "bvwbjplbgvbhsrlpgdmjqwftvncz",
+				msg:       "bvwbjplbgvbhsrlpgdmjqwftvncz",
+				markerLen: 4,
 			},
 			want: 5,
 		},
 		{
 			name: "",
 			args: args{
-				msg: "nppdvjthqldpwncqszvftbrmjlhg",
+				msg:       "nppdvjthqldpwncqszvftbrmjlhg",
+				markerLen: 4,
 			},
 			want: 6,
 		},
 		{
 			name: "",
 			args: args{
-				msg: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+				msg:       "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+				markerLen: 4,
 			},
 			want: 10,
 		},
 		{
 			name: "",
 			args: args{
-				msg: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+				msg:       "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+				markerLen: 4,
 			},
 			want: 11,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := findMarker(tt.args.msg); got != tt.want {
+			if got := findMarker(tt.args.msg, tt.args.markerLen); got != tt.want {
 				t.Errorf("findMarker() = %v, want %v", got, tt.want)
 			}
 		})
