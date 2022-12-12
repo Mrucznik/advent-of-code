de4fa645cdc992190227fb72cd4d505984a5eb21
Mrucznik <mrucznix@gmail.com>
2022-12-12 23:40:36 +0100

    day 12 part 1 solved finally & 2

--- day12/part1/main.go
+++ day12/part1/main.go
@@ -4,81 +4,122 @@ import (
 	_ "embed"
 	"fmt"
 	"github.com/RyanCarrier/dijkstra"
+	"math"
 	"strings"
 )
+
+// time needed: 1h 44min
+
+// time needed: 5min 30s
 
 //go:embed input.txt
 var input string
 
-func main() {
-	rows := strings.Split(input, "\n")
+type xy struct {
+	x int
+	y int
+}
 
-	SID := 0
-	EID := 0
+var rows []string
+
+func main() {
+	rows = strings.Split(input, "\n")
+
+	SID := []string{}
+	EID := ""
 
 	graph := dijkstra.NewGraph()
 	// find begginning nodes
 	for y, row := range rows {
 		for x, letter := range row {
 			id := getID(x, y)
-			if letter == 'S' {
-				SID = id
-			}
-			if letter == 'E' {
+
+			if letter == 'a' {
+				SID = append(SID, id)
+			} else if letter == 'E' {
 				EID = id
 			}
-			graph.AddVertex(id)
+
+			graph.AddMappedVertex(id)
 		}
 	}
 
 	// add arcs
 	for myY, row := range rows {
-		if myY+1 >= len(rows) {
-			break
-		}
 		for myX, letter := range row {
-			if myX+1 >= len(row) {
-				break
-			}
-
 			// right and down
-			if isAccessible(letter, rune(rows[myY][myX+1])) {
-				fmt.Printf("x %c %c\n", letter, rune(rows[myY][myX+1]))
+			if myX+1 < len(row) {
 				myID, neighbourID := getID(myX, myY), getID(myX+1, myY)
-				checkErr(graph.AddArc(myID, neighbourID, 1))
-				checkErr(graph.AddArc(neighbourID, myID, 1))
+				if isAccessible(letter, rune(rows[myY][myX+1])) {
+					checkErr(graph.AddMappedArc(myID, neighbourID, 1))
+				}
+				if isAccessible(rune(rows[myY][myX+1]), letter) {
+					checkErr(graph.AddMappedArc(neighbourID, myID, 1))
+				}
 			}
-			if isAccessible(letter, rune(rows[myY+1][myX])) {
-				fmt.Printf("y %c %c\n", letter, rune(rows[myY][myX+1]))
+			if myY+1 < len(rows) {
 				myID, neighbourID := getID(myX, myY), getID(myX, myY+1)
-				checkErr(graph.AddArc(myID, neighbourID, 1))
-				checkErr(graph.AddArc(neighbourID, myID, 1))
+				if isAccessible(letter, rune(rows[myY+1][myX])) {
+					checkErr(graph.AddMappedArc(myID, neighbourID, 1))
+				}
+				if isAccessible(rune(rows[myY+1][myX]), letter) {
+					checkErr(graph.AddMappedArc(neighbourID, myID, 1))
+				}
 			}
 		}
 	}
 
-	shortest, err := graph.Shortest(SID, EID)
-	if err != nil {
-		panic(err)
+	var min int64 = math.MaxInt64
+	for _, rawSID := range SID {
+		sid, err := graph.GetMapping(rawSID)
+		if err != nil {
+			panic(err)
+		}
+		eid, err := graph.GetMapping(EID)
+		if err != nil {
+			panic(err)
+		}
+
+		shortest, err := graph.Shortest(sid, eid)
+		if err != nil {
+			continue
+		}
+
+		if shortest.Distance < min {
+			min = shortest.Distance
+		}
 	}
 
-	fmt.Printf("%+v\n", shortest)
+	fmt.Printf("%+v\n", min)
+	//for _, v := range shortest.Path {
+	//	fmt.Println(graph.GetMapped(v))
+	//}
 }
 
 func isAccessible(a, b rune) bool {
-	if a == 'S' || a == 'E' || b == 'S' || b == 'E' {
-		return true
+	if a == 'S' {
+		a = 'a' - 1
+	}
+	if b == 'S' {
+		b = 'a' - 1
 	}
 
-	x := a - b
-	if x < -1 || x > 1 {
+	if a == 'E' {
+		a = 'z' + 1
+	}
+	if b == 'E' {
+		b = 'z' + 1
+	}
+
+	x := b - a
+	if x > 1 {
 		return false
 	}
 	return true
 }
 
-func getID(x, y int) int {
-	return x*1000 + y
+func getID(x, y int) string {
+	return fmt.Sprintf("%dx%d_%c", x, y, rows[y][x])
 }
 
 func checkErr(err error) {
