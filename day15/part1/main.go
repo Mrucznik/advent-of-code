ae09a4bdfc22fb8af2c6454bf003f331aadc103f
Mrucznik <mrucznix@gmail.com>
2022-12-16 18:50:28 +0100

    day 15 part 2

--- day15/part1/main.go
+++ day15/part1/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"math"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // part 1 took 34min
 
-// part 2 took
+// part 2 took 1h 24min
 
 //go:embed input.txt
 var input string
@@ -22,16 +22,16 @@ type Point struct {
 type Sensor struct {
 	Point
 	beaconDistance int
+	distX, distY   int
 }
 
 func main() {
 	re := regexp.MustCompile("Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)")
 
-	beacons := []Point{}
-	sensors := []Sensor{}
+	beacons := []*Point{}
+	sensors := []*Sensor{}
 
-	maxX := 0
-	minX := 0
+	max := 4000000
 
 	rows := strings.Split(input, "\n")
 	for _, row := range rows {
@@ -45,43 +45,79 @@ func main() {
 		by, _ := strconv.Atoi(raw[4])
 		bp := Point{bx, by}
 
-		dist := distance(sp, bp)
-		sensors = append(sensors, Sensor{sp, dist})
-		beacons = append(beacons, bp)
-
-		if sx+dist > maxX {
-			maxX = sx + dist + 1
-		}
-
-		if sx-dist < minX {
-			minX = sx - dist - 1
-		}
+		distX := Abs(bp.x - sp.x)
+		distY := Abs(bp.y - sp.y)
+		dist := distY + distX
+		sensors = append(sensors, &Sensor{sp, dist, distX, distY})
+		beacons = append(beacons, &bp)
 	}
 
-	y := 2000000
-	pos := 0
-	for x := minX - 10; x < maxX+10; x++ {
-		for _, sensor := range sensors {
-			checkPoint := Point{x, y}
-			dist := distance(checkPoint, sensor.Point)
+	pointsToCheck := make(chan Point, 100000)
 
+	wg := &sync.WaitGroup{}
+	for _, sensor := range sensors {
+		wg.Add(2)
+		go func(sensor *Sensor) {
+			for x := (-sensor.beaconDistance) - 1; x < sensor.beaconDistance+1; x++ {
+				y := Abs(sensor.beaconDistance + 1 - x)
+
+				point := Point{sensor.x + x, sensor.y - y}
+				if point.x < 0 || point.x > max || point.y < 0 || point.y > max {
+					continue
+				}
+				pointsToCheck <- point
+			}
+			wg.Done()
+		}(sensor)
+
+		go func(sensor *Sensor) {
+			for x := (-sensor.beaconDistance) - 1; x < sensor.beaconDistance+1; x++ {
+				y := Abs(sensor.beaconDistance + 1 - x)
+
+				point := Point{sensor.x + x, sensor.y + y}
+				if point.x < 0 || point.x > max || point.y < 0 || point.y > max {
+					continue
+				}
+				pointsToCheck <- point
+			}
+			wg.Done()
+		}(sensor)
+	}
+
+	fmt.Println("checking")
+	for point := range pointsToCheck {
+		otherSensorOverlap := false
+		for _, sensor := range sensors {
+			dist := Abs(sensor.y-point.y) + Abs(sensor.x-point.x)
+
+			if dist <= sensor.beaconDistance {
+				otherSensorOverlap = true
+				break
+			}
+		}
+
+		if !otherSensorOverlap {
+			beaconOverlap := false
 			for _, beacon := range beacons {
-				if checkPoint.x == beacon.x && checkPoint.y == beacon.y {
-					goto next
+				if point.x == beacon.x && point.y == beacon.y {
+					beaconOverlap = true
+					break
 				}
 			}
 
-			if dist <= sensor.beaconDistance {
-				pos++
-				break
+			if !beaconOverlap {
+				fmt.Println("wynik: ", point.x*4000000+point.y)
+				return
 			}
-		next:
 		}
 	}
-
-	fmt.Println(pos)
+	fmt.Println("1")
+	wg.Wait()
+	fmt.Println("nothing")
 }
-
-func distance(a Point, b Point) int {
-	return int(math.Abs(float64(b.x-a.x))) + int(math.Abs(float64(b.y-a.y)))
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+	return n
 }
