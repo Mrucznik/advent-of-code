dd095224400df25b084df49729f369ee93f83f66
Mrucznik <mrucznix@gmail.com>
2022-12-24 00:36:11 +0100

    day 17 part 1

--- day17/part1/main.go
+++ day17/part1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"strings"
 )
 
@@ -10,10 +11,9 @@ var input string
 
 const maxY, maxX = 100000, 7
 
-var space = [maxY][maxX]uint8{}
+var space = [maxX][maxY]uint8{}
 
 func main() {
-
 	rocks := []*Rock{
 		NewRock([4][4]uint8{ // horizontal line
 			{1, 1, 1, 1},
@@ -47,8 +47,38 @@ func main() {
 		}),
 	}
 
-	rows := strings.Split(input, "\n")
-	for _, row := range rows {
-
+	for i, rock := range rocks {
+		fmt.Printf("rock %d width: %d height %d\n", i, rock.width, rock.height)
 	}
+
+	rows := strings.Split(input, "\n")
+	allMoves := len(rows[0])
+	moves := make([]bool, allMoves)
+	for i, move := range rows[0] {
+		moves[i] = rune(move) == '<'
+	}
+
+	currMove := 0
+	currRock := 0
+	fallen := 0
+	rock := rocks[currRock]
+	rock.spawn()
+	for {
+		rock.move(moves[currMove])
+		currMove = (currMove + 1) % allMoves
+
+		if rock.fall() {
+			fallen++
+			fmt.Printf("%d rock fallen %s\n", fallen, rock)
+			currRock = (currRock + 1) % len(rocks)
+			rock = rocks[currRock]
+			rock.spawn()
+		}
+
+		if fallen == 2022 {
+			break
+		}
+	}
+
+	fmt.Println(findSpaceBottom())
 }
--- day17/part1/rock.go
+++ day17/part1/rock.go
@@ -1,59 +1,127 @@
 package main
 
+import "fmt"
+
+const rockGridSize = 4
+
+type RockGrid [rockGridSize][rockGridSize]uint8
+
 type Rock struct {
 	x, y      int
-	height int
-	rockShape [4][4]uint8
+	height    int
+	width     int
+	rockShape RockGrid
 }
 
-func NewRock(rockShape [4][4]uint8) *Rock {
+func NewRock(rockShape RockGrid) *Rock {
 	h := 0
-	for y := 0; y < 4; y++ {
-		for x := 0; x < 4; x++ {
+	for y := 0; y < rockGridSize; y++ {
+		for x := 0; x < rockGridSize; x++ {
 			if rockShape[y][x] == 1 {
 				h++
 				break
 			}
 		}
 	}
-	return &Rock{rockShape: rockShape, height: h}
+
+	w := 0
+	for x := 0; x < rockGridSize; x++ {
+		for y := 0; y < rockGridSize; y++ {
+			if rockShape[y][x] == 1 {
+				w++
+				break
+			}
+		}
+	}
+	return &Rock{rockShape: rockShape, height: h, width: w}
 }
 
 func (r *Rock) spawn() {
 	bottom := findSpaceBottom()
-	r.y = bottom + 3 + r.height
+	r.y = bottom + 3 + r.height - 1
 	r.x = 2
+	fmt.Println(bottom, "spawned", r)
 }
 
 var lastBottom int
 
 func findSpaceBottom() int {
-	bottom := lastBottom
-	for y := bottom; y < maxY; y++ {
+	for y := lastBottom; y < maxY; y++ {
+		nextRow := false
 		for x := 0; x < maxX; x++ {
-			if space[y][x] == 1 {
-				goto next
+			if space[x][y] == 1 {
+				nextRow = true
+				fmt.Println("lol", y, x)
+				break
 			}
 		}
-		return bottom
-	next:
-		bottom++
+
+		if !nextRow {
+			lastBottom = y
+			return y
+		}
 	}
-	return bottom
+	panic("no")
 }
 
 func (r *Rock) move(left bool) {
+	// collide with walls
+	if r.x <= 0 && left {
+		return
+	}
 
+	if r.x+r.width >= 7 && !left {
+		return
+	}
+
+	if left {
+		r.x--
+		if r.collide() {
+			r.x++
+		}
+	} else {
+		r.x++
+		if r.collide() {
+			r.x--
+		}
+	}
 }
 
 func (r *Rock) fall() (stop bool) {
-	if r.y == findSpaceBottom() {
+	if r.y-r.height+1 <= 0 {
 		r.place()
+		return true
 	}
+	r.y--
+	if r.collide() {
+		r.y++
+		r.place()
+		return true
+	}
+	return false
 }
 
 func (r *Rock) place() {
-	for x := 0; x < ; x++ {
-		
+	for x := 0; x < rockGridSize; x++ {
+		for y := 0; y < rockGridSize; y++ {
+			if r.rockShape[y][x] == 1 {
+				space[r.x+x][r.y-y] = 1
+			}
+		}
 	}
+}
+
+func (r *Rock) collide() bool {
+	for x := 0; x < rockGridSize; x++ {
+		for y := 0; y < rockGridSize; y++ {
+			if r.rockShape[y][x] == 1 && space[r.x+x][r.y-y] == 1 {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (r *Rock) String() string {
+	return fmt.Sprintf("rock x: %d y: %d width: %d height: %d", r.x, r.y, r.width, r.height)
 }
