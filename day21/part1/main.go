2ff1090daf138a8039c39730e5bcd5e28f9de348
Mrucznik <mrucznix@gmail.com>
2022-12-27 21:03:11 +0100

    day 21 part 1

--- day21/part1/main.go
+++ day21/part1/main.go
@@ -1,10 +1,63 @@
 package main
 
-import _ "embed"
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// part 1 time needed: 12min 30s
+// part 2 time needed:
 
 //go:embed input.txt
 var input string
 
 func main() {
+	chans := map[string]chan int{}
 
+	for _, row := range strings.Split(input, "\n") {
+		raw := strings.Split(row, ": ")
+		monkey := raw[0]
+		equation := raw[1]
+
+		if val, err := strconv.Atoi(equation); err == nil {
+			if _, ok := chans[monkey]; !ok {
+				chans[monkey] = make(chan int)
+			}
+
+			go func(monke string) {
+				chans[monkey] <- val
+			}(monkey)
+			continue
+		} else {
+			rawEq := strings.Split(equation, " ")
+			m1, eq, m2 := rawEq[0], rawEq[1], rawEq[2]
+			if _, ok := chans[monkey]; !ok {
+				chans[monkey] = make(chan int)
+			}
+			if _, ok := chans[m1]; !ok {
+				chans[m1] = make(chan int)
+			}
+			if _, ok := chans[m2]; !ok {
+				chans[m2] = make(chan int)
+			}
+
+			go func(monke, m1, eq, m2 string) {
+				a, b := <-chans[m1], <-chans[m2]
+				switch eq {
+				case "*":
+					chans[monke] <- a * b
+				case "/":
+					chans[monke] <- a / b
+				case "+":
+					chans[monke] <- a + b
+				case "-":
+					chans[monke] <- a - b
+				}
+			}(monkey, m1, eq, m2)
+		}
+	}
+
+	fmt.Println(<-chans["root"])
 }
