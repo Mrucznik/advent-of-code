6e513086dc7383e62593cae38f1952710324b627
Mrucznik <mrucznix@gmail.com>
2022-12-27 21:58:33 +0100

    day 21 part 2 bruteforce

--- day21/part1/main.go
+++ day21/part1/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // part 1 time needed: 12min 30s
@@ -14,7 +15,7 @@ import (
 var input string
 
 func main() {
-	chans := map[string]chan int{}
+	chans := &sync.Map{}
 
 	for _, row := range strings.Split(input, "\n") {
 		raw := strings.Split(row, ": ")
@@ -22,42 +23,87 @@ func main() {
 		equation := raw[1]
 
 		if val, err := strconv.Atoi(equation); err == nil {
-			if _, ok := chans[monkey]; !ok {
-				chans[monkey] = make(chan int)
+			if _, ok := chans.Load(monkey); !ok {
+				chans.Store(monkey, make(chan int))
 			}
 
 			go func(monke string) {
-				chans[monkey] <- val
+				for {
+					c, _ := chans.Load(monke)
+					c.(chan int) <- val
+				}
 			}(monkey)
 			continue
 		} else {
 			rawEq := strings.Split(equation, " ")
 			m1, eq, m2 := rawEq[0], rawEq[1], rawEq[2]
-			if _, ok := chans[monkey]; !ok {
-				chans[monkey] = make(chan int)
+			if _, ok := chans.Load(monkey); !ok {
+				chans.Store(monkey, make(chan int))
 			}
-			if _, ok := chans[m1]; !ok {
-				chans[m1] = make(chan int)
+			if _, ok := chans.Load(m1); !ok {
+				chans.Store(m1, make(chan int))
 			}
-			if _, ok := chans[m2]; !ok {
-				chans[m2] = make(chan int)
+			if _, ok := chans.Load(m2); !ok {
+				chans.Store(m2, make(chan int))
 			}
 
 			go func(monke, m1, eq, m2 string) {
-				a, b := <-chans[m1], <-chans[m2]
-				switch eq {
-				case "*":
-					chans[monke] <- a * b
-				case "/":
-					chans[monke] <- a / b
-				case "+":
-					chans[monke] <- a + b
-				case "-":
-					chans[monke] <- a - b
+				for {
+					c1, _ := chans.Load(m1)
+					c2, _ := chans.Load(m2)
+
+					a, b := <-c1.(chan int), <-c2.(chan int)
+					switch eq {
+					case "*":
+						c, _ := chans.Load(monke)
+						c.(chan int) <- a * b
+					case "/":
+						c, _ := chans.Load(monke)
+						c.(chan int) <- a / b
+					case "+":
+						c, _ := chans.Load(monke)
+						c.(chan int) <- a + b
+					case "-":
+						c, _ := chans.Load(monke)
+						c.(chan int) <- a - b
+					case "=":
+						if a == b {
+							c, _ := chans.Load(monke)
+							c.(chan int) <- 1
+						} else {
+							c, _ := chans.Load(monke)
+							c.(chan int) <- 0
+						}
+					}
 				}
 			}(monkey, m1, eq, m2)
 		}
 	}
 
-	fmt.Println(<-chans["root"])
+	go func() {
+		i := 100000
+		defer func() {
+			if r := recover(); r != nil {
+				fmt.Println("aaa", i)
+			}
+		}()
+		for {
+			c, _ := chans.Load("humn")
+			c.(chan int) <- i
+			i++
+		}
+
+	}()
+
+	i := 0
+	for {
+		c, _ := chans.Load("humn")
+		rc, _ := chans.Load("root")
+		if <-rc.(chan int) == 1 {
+			fmt.Println("wynik", i)
+			close(c.(chan int))
+			break
+		}
+		i++
+	}
 }
