d9a5e70075c6074996e421999490add27a377547
Mrucznik <mrucznix@gmail.com>
2022-12-27 12:46:20 +0100

    day 18 part 1

--- day18/part1/main.go
+++ day18/part1/main.go
@@ -1,10 +1,56 @@
 package main
 
-import _ "embed"
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 //go:embed input.txt
 var input string
 
-func main() {
+type Pos struct {
+	x, y, z int
+}
 
+var space = [100][100][100]bool{}
+
+func main() {
+	rows := strings.Split(input, "\n")
+
+	droplets := map[Pos]struct{}{}
+	for _, row := range rows {
+		coords := strings.Split(row, ",")
+		x, _ := strconv.Atoi(coords[0])
+		y, _ := strconv.Atoi(coords[1])
+		z, _ := strconv.Atoi(coords[2])
+
+		space[x][y][z] = true
+		droplets[Pos{x, y, z}] = struct{}{}
+	}
+
+	sides := 0
+	for pos := range droplets {
+		// count sides
+		sides += checkSide(pos, 0, 0, 1)
+		sides += checkSide(pos, 0, 1, 0)
+		sides += checkSide(pos, 1, 0, 0)
+		sides += checkSide(pos, 0, 0, -1)
+		sides += checkSide(pos, 0, -1, 0)
+		sides += checkSide(pos, -1, 0, 0)
+	}
+
+	fmt.Println(sides)
+}
+
+func checkSide(pos Pos, tx, ty, tz int) int {
+	if pos.x+tx < 0 || pos.y+ty < 0 || pos.z+tz < 0 {
+		return 1
+	}
+
+	if space[pos.x+tx][pos.y+ty][pos.z+tz] {
+		return 0
+	}
+	return 1
 }
