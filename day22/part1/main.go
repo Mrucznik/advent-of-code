bab3b9b6dd68c1544165f9adf047b9a338e1f181
Mrucznik <mrucznix@gmail.com>
2022-12-28 01:15:09 +0100

    day 22 part 1 bugged

--- day22/part1/main.go
+++ day22/part1/main.go
@@ -1,10 +1,149 @@
 package main
 
-import _ "embed"
+import (
+	_ "embed"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
 
 //go:embed input.txt
 var input string
 
-func main() {
+type Ja struct {
+	x, y, facing int
+}
 
+const maxX, maxY = 150, 200
+
+func (ja *Ja) nextCoord(times int) (int, int) {
+	if ja.facing == 0 { // right
+		return (ja.x + times) % maxX, ja.y
+	} else if ja.facing == 1 { // down
+		return ja.x, (ja.y + times) % maxY
+	} else if ja.facing == 2 { // left
+		x := ja.x - times
+		if x < 0 {
+			x += maxX
+		}
+		return x, ja.y
+	} else if ja.facing == 3 { // up
+		y := ja.y - times
+		if y < 0 {
+			y += maxY
+		}
+		return ja.x, y
+	}
+	panic("no")
+}
+
+func (ja *Ja) L() {
+	if ja.facing == 0 {
+		ja.facing = 3
+		return
+	}
+	ja.facing = ja.facing - 1
+}
+
+func (ja *Ja) R() {
+	if ja.facing == 3 {
+		ja.facing = 0
+		return
+	}
+	ja.facing = ja.facing + 1
+}
+
+func (ja *Ja) move(times int) {
+	spaces := 0
+	for i := 0; i < times; {
+		x, y := ja.nextCoord(1 + i + spaces)
+		moves[y][x] = ja.facing + 1
+		tile := space[y][x]
+		if tile == ' ' {
+			spaces++
+			continue
+		}
+
+		if tile == '#' {
+			// back
+			for j := 0; j <= spaces; j++ {
+				x, y = ja.nextCoord(i + spaces - j)
+				if space[y][x] != ' ' {
+					ja.x, ja.y = x, y
+					return
+				}
+			}
+		}
+
+		i++
+	}
+	ja.x, ja.y = ja.nextCoord(times + spaces)
+}
+
+func (ja Ja) String() string {
+	return fmt.Sprintf("x: %d, y: %d, facing: %d", ja.x, ja.y, ja.facing)
+}
+
+var space = [maxY][maxX]rune{}
+var moves = [maxY][maxX]int{}
+
+func main() {
+	rows := strings.Split(input, "\n")
+
+	// load map
+	for y := 0; y < maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			if x >= len(rows[y]) {
+				space[y][x] = ' '
+			} else {
+				space[y][x] = rune(rows[y][x])
+			}
+		}
+	}
+
+	// sequences
+	sequences := rows[maxY+1]
+	fmt.Println(sequences)
+
+	ja := Ja{0, 0, 0}
+
+	re := regexp.MustCompile("(\\d+)([LR])")
+	matches := re.FindAllStringSubmatch(sequences, -1)
+	for _, match := range matches {
+		steps, err := strconv.Atoi(match[1])
+		if err != nil {
+			panic(err)
+		}
+		turn := match[2]
+
+		fmt.Println(steps, turn)
+
+		// do the move
+		ja.move(steps)
+		if turn == "L" {
+			ja.L()
+		} else {
+			ja.R()
+		}
+		fmt.Println(ja)
+
+		for i := 0; i < maxY; i++ {
+			for j := 0; j < maxX; j++ {
+				if moves[i][j] > 0 {
+					fmt.Printf("\033[%dm%c\033[0m", 30+moves[i][j], space[i][j])
+				} else {
+					fmt.Print(string(space[i][j]))
+				}
+			}
+			fmt.Println()
+		}
+		time.Sleep(10 * time.Second)
+	}
+
+	fmt.Println(1000*(ja.y+1) + 4*(ja.x+1) + ja.facing)
+
+	// 8224
+	// 27464 to low
 }
